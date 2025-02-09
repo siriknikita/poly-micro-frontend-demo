354c9e037e721987d411fb4ff86344b46d694c86
siriknikita <mykyta.siryk@lnu.edu.ua>
2025-02-09 18:04:37 +0200

    feat: add pretty underline for the topbar

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -21,8 +21,7 @@ export const TopBar: React.FC<TopBarProps> = ({
   onLogout,
 }) => {
   return (
-    <nav className="sticky top-0 z-10 bg-white dark:bg-gray-800 shadow">
-      {/* TODO: use `border` instead of `shadow` */}
+    <nav className="sticky top-0 z-10 bg-white dark:bg-gray-800 border-b border-gray-300 dark:border-gray-700">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center space-x-4">
